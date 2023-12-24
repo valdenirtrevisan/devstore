23e63b4c8d38c50593e02de1b85ed26d79bc6e02
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2023-12-24 00:48:43 -0300

    alterado home para pegar os dados da api

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,7 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(path, baseUrl)
+  const url = new URL('/api'.concat(path), baseUrl)
 
   return fetch(url, init)
 }
