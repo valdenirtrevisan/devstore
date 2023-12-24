a4d02f5d1c622c7c4bbff81b8f0a83fb5e71bf7c
Valdenir Trevisan Junior <valdenirtrevi@gmail.com>
2023-12-24 00:56:16 -0300

    criando um loading para home

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,7 +1,11 @@
+import { Skeleton } from '@/components/skeleton'
+
 export default function DefaultLoading() {
   return (
-    <div>
-      <p>Carregando...</p>
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[662px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
     </div>
   )
 }
--- src/data/api.ts
+++ src/data/api.ts
@@ -1,6 +1,8 @@
 import { env } from '@/env'
 
-export function api(path: string, init?: RequestInit) {
+export async function api(path: string, init?: RequestInit) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
   const url = new URL('/api'.concat(path), baseUrl)
 
